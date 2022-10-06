5a4550f7ff32d6e490f93e1a642bbf39a0ab3995
Ferdinand Maulana Za Fauzi <101077118+Ferdinand-MZ@users.noreply.github.com>
2022-10-06 09:13:50 +0700

    Add files via upload

---
+++ sketch_push_button_2.ino
@@ -0,0 +1,29 @@
+int button= 2;
+int nilaitombol;
+int count;
+
+int led= 8;
+int led2= 12;
+
+void setup(){
+pinMode(button, INPUT);
+pinMode(led, OUTPUT);
+}
+
+void loop(){
+nilaitombol= digitalRead(button);
+
+if(nilaitombol == 1){
+count++;
+delay(300);
+if(count==1){
+digitalWrite(led, HIGH);
+digitalWrite(led2, LOW);
+}
+if(count==2){
+digitalWrite(led, LOW);
+digitalWrite(led2 , HIGH);
+count=0;
+}
+}
+}
